b1fb1a74789ddaa3f197a575c47fe8457ad3d3e5
stsmith1991 <38431753+stsmith1991@users.noreply.github.com>
2020-10-03 16:38:05 +0500

    Домашнее задание по теме "типы данных"

---
+++ екнещутгь.py
@@ -0,0 +1,5 @@
+[number1, number2, number3, number4, number5, number6, number7, number8, numeber9, number10] = list(map(list, input("Введите числа:").split()))
+words=list([number1, number2, number3, number4, number5, number6, number7, number8, numeber9, number10])
+x=int()
+for int(x) in words:
+    words[int(x)]=words[int(x)].splice[0,11]
---
+++ словарные_приколы.py
@@ -0,0 +1,8 @@
+words=list()
+num=int()
+while len(words)!=10:
+    for num in "word":
+        words[int(num)]=list(input('Введите слово'))
+print(words)
+for int(x) in words:
+    words[int(x)]=words(str([int(x)].splice[0,11]))
